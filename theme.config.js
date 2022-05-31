73d760cca006738e0bfa0e699cc42c21526e10c8
Guilherme de Jesus <71342156+Allzon@users.noreply.github.com>
2022-05-31 17:39:04 -0300

    full v1

--- theme.config.js
+++ theme.config.js
@@ -1,12 +1,14 @@
 export default {
   github: 'https://github.com/shuding/nextra',
   docsRepositoryBase: 'https://github.com/shuding/nextra/blob/master',
-  titleSuffix: ' – Nextra',
+  titleSuffix: ' – DCAMH',
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
+      <span className="mr-2 font-extrabold hidden md:inline">
+        Diretório Acadêmico{' '}
+      </span>
       <span className="text-gray-600 font-normal hidden md:inline">
-        The Next.js Static Site Generator
+        Margaret Hamilton
       </span>
     </>
   ),
